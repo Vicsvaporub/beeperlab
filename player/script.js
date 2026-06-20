835cca828962597e9d91279b40c4743c05ca4f34
Vicsvaporub <paulawong09@gmail.com>
2026-06-19 21:18:52 -0500

    Update script.js

--- player/script.js
+++ player/script.js
@@ -1,1 +1,51 @@
+const songs = [
+    {
+        title: "The Burn Marks on My Epiano Won't Go Away",
+        artist: "A L E X",
+        file: "../music/A L E X - The Burn Marks on My Epiano Wont Go Away.mp3"
+    },
+    {
+        title: "Space Song",
+        artist: "Beach House",
+        file: "../music/Beach House - Space Song.mp3"
+    },
+    {
+        title: "Hollow Log",
+        artist: "Beck",
+        file: "../music/Beck - Hollow Log.mp3"
+    },
+    {
+        title: "Recuerdos De Ypacaraí",
+        artist: "Caetano Veloso",
+        file: "../music/Caetano Veloso - Recuerdos De Ypacarai.mp3"
+    },
+    {
+        title: "Tonada De Luna Llena",
+        artist: "Caetano Veloso",
+        file: "../music/Caetano Veloso - Tonada De Luna Llena.mp3"
+    },
+    {
+        title: "Duvet",
+        artist: "bôa",
+        file: "../music/bôa - Duvet.mp3"
+    }
+];
 
+const playlist = document.getElementById("playlist");
+const audio = document.getElementById("audio");
+
+songs.forEach((song) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <strong>${song.title}</strong><br>
+        <small>${song.artist}</small>
+    `;
+
+    li.onclick = () => {
+        audio.src = song.file;
+        audio.play();
+    };
+
+    playlist.appendChild(li);
+});
